a44d6c0684c695b150cb0e53ce36fcaf9e37d734
mrubart <mrubart@gmail.com>
2014-12-29 14:50:22 -0700

    yay

---
+++ public/app/app.js
@@ -0,0 +1,8 @@
+﻿angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+    .when('/', { templateUrl: '/partials/main', controller: 'hfMainCtrl' })
+});
+
